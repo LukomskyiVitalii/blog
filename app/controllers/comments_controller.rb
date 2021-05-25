f3cb5fbb3f2782cbb6d935d31b21301562b4a537
LukomskyiVitalii <vitalii.lukomskyi.pz.2019@lpnu.ua>
2021-05-25 14:51:04 +0300

    aded all neded links

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
     def create
       @article = Article.find(params[:article_id])
       @comment = @article.comments.create(comment_params)
+      @comment.user = current_user
+      @comment.save
       redirect_to article_path(@article)
     end
   
@@ -14,7 +16,7 @@ class CommentsController < ApplicationController
   
     private
       def comment_params
-        params.require(:comment).permit(:commenter, :body)
+        params.require(:comment).permit(:body)
       end
   end
   
--- db/migrate/20210525091921_add_user_to_article.rb
+++ db/migrate/20210525091921_add_user_to_article.rb
@@ -1,5 +1,5 @@
 class AddUserToArticle < ActiveRecord::Migration[6.1]
   def change
-    add_reference :articles, :user, foreign_key: true
+    add_reference :articles, :user, foreign_key: {to_table: :users}
   end
 end
